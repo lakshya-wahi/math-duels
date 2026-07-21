f9799092969f8a4bdce86f8852fc12a750b4a46a
Lakshya Wahi <142634369+lakshya-wahi@users.noreply.github.com>
2026-07-21 13:46:20 -0700

    Update ItemCatalog.lua

--- src/shared/ItemCatalog.lua
+++ src/shared/ItemCatalog.lua
@@ -1,1 +1,169 @@
+local ItemCatalog = {
+	Avatars = {
+		Default = {
+			DisplayName = "Default Avatar",
+			Price = 0,
+			Image = "rbxassetid://117284685501723",
+			CardImage = "rbxassetid://103458469395511",
+		},
 
+		Duelist = {
+			DisplayName = "Duelist Avatar",
+			Price = 2500,
+			Image = "rbxassetid://111725742098477",
+			CardImage = "rbxassetid://103458469395511",
+		},
+
+		Warrior = {
+			DisplayName = "Warrior Avatar",
+			Price = 5000,
+			Image = "rbxassetid://127060971799457",
+			CardImage = "rbxassetid://103458469395511",
+		},
+
+		Combatant = {
+			DisplayName = "Combatant Avatar",
+			Price = 6500,
+			Image = "rbxassetid://117420377121275",
+			CardImage = "rbxassetid://103458469395511",
+		},
+
+		Royal = {
+			DisplayName = "Royal Avatar",
+			Price = 9000,
+			Image = "rbxassetid://76679884784971",
+			CardImage = "rbxassetid://103458469395511",
+		},
+
+		General = {
+			DisplayName = "General Avatar",
+			Price = 15000,
+			Image = "rbxassetid://98135494106816",
+			CardImage = "rbxassetid://103458469395511",
+		},
+
+		Cyber = {
+			DisplayName = "Cyber Avatar",
+			Price = 30000,
+			Image = "rbxassetid://83723836364863",
+			CardImage = "rbxassetid://103458469395511",
+		},
+
+		Overlord = {
+			DisplayName = "Overlord Avatar",
+			Price = 50000,
+			Image = "rbxassetid://72272625483272",
+			CardImage = "rbxassetid://103458469395511",
+		},
+	},
+
+	Banners = {
+		Basic = {
+			DisplayName = "Basic Banner",
+			Price = 0,
+			Image = "rbxassetid://84473683038801",
+			CardImage = "rbxassetid://106691180101617",
+		},
+
+		Whirlwind = {
+			DisplayName = "Whirlwind Banner",
+			Price = 1500,
+			Image = "rbxassetid://137414300651841",
+			CardImage = "rbxassetid://106691180101617",
+		},
+
+		Zen = {
+			DisplayName = "Zen Banner",
+			Price = 5000,
+			Image = "rbxassetid://133264049878016",
+			CardImage = "rbxassetid://106691180101617",
+		},
+
+		Blazing = {
+			DisplayName = "Blazing Banner",
+			Price = 8000,
+			Image = "rbxassetid://128469927865199",
+			CardImage = "rbxassetid://106691180101617",
+		},
+
+		Acidic = {
+			DisplayName = "Acidic Banner",
+			Price = 11000,
+			Image = "rbxassetid://95864637765354",
+			CardImage = "rbxassetid://106691180101617",
+		},
+
+		Meteoric = {
+			DisplayName = "Meteoric Banner",
+			Price = 18000,
+			Image = "rbxassetid://111245501893530",
+			CardImage = "rbxassetid://106691180101617",
+		},
+
+		Electric = {
+			DisplayName = "Electric Banner",
+			Price = 32500,
+			Image = "rbxassetid://124389605555176",
+			CardImage = "rbxassetid://106691180101617",
+		},
+
+		Celestial = {
+			DisplayName = "Celestial Banner",
+			Price = 40000,
+			Image = "rbxassetid://77845575649803",
+			CardImage = "rbxassetid://106691180101617",
+		},
+	},
+
+	Emotes = {
+		Emote1 = {
+			Price = 1200,
+			Image = "rbxassetid://99975218563274",
+			CardImage = "rbxassetid://72436950214331",
+		},
+
+		Emote2 = {
+			Price = 1750,
+			Image = "rbxassetid://80082037680532",
+			CardImage = "rbxassetid://72436950214331",
+		},
+
+		Emote3 = {
+			Price = 1750,
+			Image = "rbxassetid://88072065492559",
+			CardImage = "rbxassetid://72436950214331",
+		},
+
+		Emote4 = {
+			Price = 2500,
+			Image = "rbxassetid://99079705554158",
+			CardImage = "rbxassetid://72436950214331",
+		},
+
+		Emote5 = {
+			Price = 3500,
+			Image = "rbxassetid://75652809104745",
+			CardImage = "rbxassetid://72436950214331",
+		},
+
+		Emote6 = {
+			Price = 5000,
+			Image = "rbxassetid://96658994277186",
+			CardImage = "rbxassetid://72436950214331",
+		},
+
+		Emote7 = {
+			Price = 5000,
+			Image = "rbxassetid://102559112788377",
+			CardImage = "rbxassetid://72436950214331",
+		},
+
+		Emote8 = {
+			Price = 10000,
+			Image = "rbxassetid://75351428876116",
+			CardImage = "rbxassetid://72436950214331",
+		},
+	},
+}
+
+return ItemCatalog
