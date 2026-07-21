80d1d19f71910be2ece36f94ef920691dcdf114e
Lakshya Wahi <142634369+lakshya-wahi@users.noreply.github.com>
2026-07-21 13:49:26 -0700

    Update Constants.lua

--- src/shared/Constants.lua
+++ src/shared/Constants.lua
@@ -1,1 +1,29 @@
+local Constants = {
+	DataStores = {
+		PlayerData = "PlayerData",
+	},
 
+	Matchmaking = {
+		PlayersPerMatch = 2,
+		QueueName = "MatchmakingQueue",
+	},
+
+	DefaultPlayerData = {
+		Coins = 0,
+		EquippedAvatar = "Default",
+		EquippedBanner = "Basic",
+		EquippedEmote = "Emote1",
+	},
+
+	Remotes = {
+		JoinQueue = "JoinQueue",
+		LeaveQueue = "LeaveQueue",
+		UpdateQueue = "UpdateQueue",
+	},
+
+	Teleport = {
+		MatchPlaceId = 0, -- replace with the actual destination PlaceId
+	},
+}
+
+return table.freeze(Constants)
