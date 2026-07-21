cfd2f9a7cffd8df267014810a01b2917f844a21e
Lakshya Wahi <142634369+lakshya-wahi@users.noreply.github.com>
2026-07-21 13:30:42 -0700

    Update PlayerController.client.lua

--- src/client/PlayerController.client.lua
+++ src/client/PlayerController.client.lua
@@ -1,1 +1,25 @@
+local SoundService  = game:GetService("SoundService")
+local TweenService  = game:GetService("TweenService")
 
+local MUSIC_ID      = "rbxassetid://134960058966518"
+local TARGET_VOLUME = 0.2
+local FADE_TIME     = 5.0
+
+local music = SoundService:FindFirstChild("LobbyMusic")
+if not music then
+	music = Instance.new("Sound")
+	music.Name = "LobbyMusic"
+	music.SoundId = MUSIC_ID
+	music.Looped = true
+	music.Volume = 0  
+	music.PlaybackSpeed = 1
+	music.RollOffMode = Enum.RollOffMode.Linear 
+	music.Parent = SoundService
+end
+
+if not music.Playing then
+	if not music.IsLoaded then music.Loaded:Wait() end
+	music:Play()
+end
+
+TweenService:Create(music, TweenInfo.new(FADE_TIME), { Volume = TARGET_VOLUME }):Play()
